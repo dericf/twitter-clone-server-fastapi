c4698c3df218f0e238dfea2e523fa3f3605d048e
deric92 <dericgf@gmail.com>
2021-03-16 22:19:34 -0600

    Add retry logic for DB connection (ensures docker container is booted)

--- api/database.py
+++ api/database.py
@@ -2,12 +2,27 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./twitter-clone.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+import time
+import os
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-) # remove connect_args for non-sqlite db
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./twitter-clone.db"
+SQLALCHEMY_DATABASE_URL = os.environ.get("POSTGRES_URL") # "postgresql://admin:secret@db:5432/twitterdb"
+
+engine = None
+SessionLocal = None
+Base = None
+
+retries = 5
+while retries > 0:
+    try:
+        engine = create_engine(
+            SQLALCHEMY_DATABASE_URL #, connect_args={"check_same_thread": False}
+        ) # remove connect_args for non-sqlite db
+        SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+        Base = declarative_base()
+        break
+    except Exception as e:
+        print("Error connecting..." + str(e))
+        retries -= 1
+        time.sleep(3)
