3b13e1972488cee1cf036197ddc45daacc1e3d4f
deric92 <dericgf@gmail.com>
2021-03-23 23:09:06 -0600

    Format & add docstrings

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,7 +1,13 @@
+# FastAPI
 from fastapi import APIRouter, HTTPException, Request, Depends
+
+# SQLAlchemy
 from sqlalchemy.orm import Session
+
+# Types
 from typing import List
 
+# Custom Modules
 from .. import schemas, crud
 from ..core import security
 from ..core.config import settings
@@ -9,8 +15,11 @@ from ..dependencies import get_db
 
 router = APIRouter(prefix="/users", tags=['users'])
 
+
 @router.post("/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """Create a new user record in the database
+    """
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
@@ -18,13 +27,15 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get("/", response_model=List[schemas.UserResponse])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def get_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """Return a list of all users in the database
+    """
     users = crud.get_users(db, skip=skip, limit=limit)
     #
     # Example of building a custom pydantic model
     #
     return [schemas.UserResponse(
-        id=user.id, 
+        id=user.id,
         email=user.email,
         username=user.username,
         bio=user.bio,
@@ -39,7 +50,9 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
 
 
 @router.get("/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    """Return a single user based on user_id primary key
+    """
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
