c30b4ba9757c4e69386369ed5a600a8bc15c8a97
deric92 <dericgf@gmail.com>
2021-03-23 23:09:26 -0600

    Fully implement /token login endpoint

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -1,22 +1,32 @@
+# FastAPI
 from fastapi import APIRouter, HTTPException, Request, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+# SQLAlchemy
 from sqlalchemy.orm import Session
+
+# Types
 from typing import List
 
+# Custom Modules
 from .. import schemas, crud
 from ..core import security
 from ..core.config import settings
 from ..dependencies import get_db
 
-router = APIRouter(prefix="/auth", tags=['auth'])
+router = APIRouter(tags=['auth'])
 
 
-@router.post('/login', response_model=schemas.LoginResponse)
-def attempt_login(request: Request, body:schemas.LoginRequest):
-    print(request.json)
-    print("Email: ")
-    print(body.email)
-    user: schemas.User = crud.get_user_by_email(email=body.email)
+@router.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    """User will attempt to authenticate with a email/password flow
+    """
+    user = security.authenticate_user(
+        db, form_data.username, form_data.password)
     if not user:
-        return schemas.LoginResponse(success=False, error=True, message="")
-    print(security.verify_password(body.password, user.password))
-    return schemas.LoginResponse(success=True)
+        # Wrong email or password provided
+        raise HTTPException(
+            status_code=400, detail="Incorrect username or password")
+
+    token = security.create_access_token(data={"sub": user.email})
+    return {"access_token": token, "token_type": "bearer"}
