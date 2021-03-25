0f63ba27d47a07fa6c9f14d3bce433acdc161dc0
deric92 <dericgf@gmail.com>
2021-03-25 01:28:50 -0600

    Add user (authenticated) delete endpoint

--- api/routers/users.py
+++ api/routers/users.py
@@ -1,5 +1,5 @@
 # FastAPI
-from fastapi import APIRouter, HTTPException, Request, Depends, BackgroundTasks
+from fastapi import APIRouter, HTTPException, status, Request, Depends, BackgroundTasks
 
 # SQLAlchemy
 from sqlalchemy.orm import Session
@@ -8,10 +8,10 @@ from sqlalchemy.orm import Session
 from typing import List
 
 # Custom Modules
-from .. import schemas, crud, dependencies
+from .. import schemas, crud
 from ..core import security
 from ..core.config import settings
-from ..dependencies import get_db
+from ..dependencies import get_db, get_current_user
 from .. import background_functions
 
 router = APIRouter(prefix="/users", tags=['users'])
@@ -53,7 +53,7 @@ def get_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
 
 
 @router.get("/me", response_model=schemas.User)
-def get_authenticated_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(dependencies.get_current_user)):
+def get_authenticated_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
     """Get the currently logged in user if there is one (testing purposes only)
     """
     return current_user
@@ -63,7 +63,18 @@ def get_authenticated_user(db: Session = Depends(get_db), current_user: schemas.
 def get_user(user_id: int, db: Session = Depends(get_db)):
     """Return a single user based on user_id primary key
     """
-    db_user = crud.get_user(db, user_id=user_id)
+    db_user = crud.get_user_by_id(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@router.delete('/', response_model=schemas.EmptyResponse)
+def delete_user(request_body:schemas.UserDeleteRequestBody,
+                 db: Session = Depends(get_db),
+                 current_user: schemas.UserWithPassword = Depends(get_current_user)):
+    if not security.verify_password(request_body.password, current_user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not authorized to delete that user")
+    delete_successful = crud.delete_user(db, current_user.id)
+    
+    return {}
