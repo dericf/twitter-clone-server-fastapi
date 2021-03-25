9e51076975f9937d45a5842c63e596a47efff2f7
deric92 <dericgf@gmail.com>
2021-03-24 23:21:55 -0600

    Add placeholder/test background function

---
+++ api/background_functions.py
@@ -0,0 +1,14 @@
+#
+# Utility functions for background tasks
+#
+async def send_registration_confirmation_email(email: str):
+    """Render the Email HTML (& PlainText) to a file
+    """
+    message: str = """ 
+    This is a test
+    """
+    # with open("log.txt", mode="a") as email_file:
+    #     content = f"notification for {email}: {message}"
+    #     email_file.write(content)
+
+    print(f'TESTING EMAIL CONFIRMATION')
