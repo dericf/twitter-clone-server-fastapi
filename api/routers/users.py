00a2616d0f1ce7c55c50cb5a911fc919591fbec9
deric92 <dericgf@gmail.com>
2021-03-26 02:10:47 -0600

    Fix user get endpoint - now combined into 1 and condensed

--- api/routers/users.py
+++ api/routers/users.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, HTTPException, status, Request, Depends, Backgrou
 from sqlalchemy.orm import Session
 
 # Types
-from typing import List
+from typing import List, Optional
 
 # Custom Modules
 from .. import schemas, crud
@@ -15,6 +15,33 @@ from ..dependencies import get_db, get_current_user
 from .. import background_functions
 
 router = APIRouter(prefix="/users", tags=['users'])
+
+@router.get("/", response_model=List[schemas.UserResponse])
+def get_all_users(userId: Optional[int] = None, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """Return a list of all users in the database
+    """
+    if userId:
+        users = [crud.get_user_by_id(db, userId)]
+    else:
+        users = crud.get_users(db, skip=skip, limit=limit)
+    
+    # TODO perhaps there is a better way of returning this model.
+    # It seems like its trying to immidate graphql
+    return [
+        schemas.UserResponse(  
+        id=user.id,
+        email=user.email,
+        username=user.username,
+        bio=user.bio,
+        birthdate=user.birthdate
+    ) for user in users]
+
+
+@router.get("/me", response_model=schemas.User)
+def get_authenticated_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
+    """Get the currently logged in user if there is one (testing purposes only)
+    """
+    return current_user
 
 
 @router.post("/", response_model=schemas.User)
@@ -27,46 +54,6 @@ async def create_user(user: schemas.UserCreate, bg_tasks: BackgroundTasks, db: S
     print("Sending registration email")
     bg_tasks.add_task(background_functions.send_registration_confirmation_email, email=user.email)
     return crud.create_user(db=db, user=user)
-
-
-@router.get("/", response_model=List[schemas.UserResponse])
-def get_all_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    """Return a list of all users in the database
-    """
-    users = crud.get_users(db, skip=skip, limit=limit)
-    #
-    # Example of building a custom pydantic model
-    #
-    return [schemas.UserResponse(
-        id=user.id,
-        email=user.email,
-        username=user.username,
-        bio=user.bio,
-        birthdate=user.birthdate,
-        tweets=[
-            schemas.BasicTweet(
-                id=tweet.id,
-                content=tweet.content
-            ) for tweet in user.tweets
-        ]
-    ) for user in users]
-
-
-@router.get("/me", response_model=schemas.User)
-def get_authenticated_user(db: Session = Depends(get_db), current_user: schemas.User = Depends(get_current_user)):
-    """Get the currently logged in user if there is one (testing purposes only)
-    """
-    return current_user
-
-
-@router.get("/{user_id}", response_model=schemas.User)
-def get_user(user_id: int, db: Session = Depends(get_db)):
-    """Return a single user based on user_id primary key
-    """
-    db_user = crud.get_user_by_id(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
 
 
 @router.put('/', response_model=schemas.UserUpdateResponseBody)
