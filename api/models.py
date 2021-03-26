1cfdf4e675aaf0c6dda23df694029ba5ed0f26d3
deric92 <dericgf@gmail.com>
2021-03-26 02:00:22 -0600

    Return orm joins back to lazy to reduce delay

--- api/models.py
+++ api/models.py
@@ -14,12 +14,12 @@ class User(Base):
     birthdate = Column(Date, index=True)
     hashed_password = Column(String, nullable=False)
     
-    tweets = relationship("Tweet", back_populates="user", lazy="joined")
-    followers = relationship("Follows", back_populates="user", foreign_keys="Follows.user_id", lazy="joined")
-    follows = relationship("Follows", back_populates="follows_user", foreign_keys="Follows.follows_user_id", lazy="joined")
-    comments = relationship("Comments", back_populates="user", lazy="joined")
-    tweet_likes = relationship("TweetLikes", back_populates="user", lazy="joined")
-    comment_likes = relationship("CommentLikes", back_populates="user", lazy="joined")
+    tweets = relationship("Tweet", back_populates="user")
+    followers = relationship("Follows", back_populates="user", foreign_keys="Follows.user_id")
+    follows = relationship("Follows", back_populates="follows_user", foreign_keys="Follows.follows_user_id")
+    comments = relationship("Comments", back_populates="user")
+    tweet_likes = relationship("TweetLikes", back_populates="user")
+    comment_likes = relationship("CommentLikes", back_populates="user")
 
 
 class Follows(Base):
@@ -31,8 +31,8 @@ class Follows(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     follows_user_id = Column(Integer, ForeignKey("users.id"))
 
-    user = relationship("User", back_populates="follows", foreign_keys=[user_id], lazy="joined")
-    follows_user = relationship("User", back_populates="followers", foreign_keys=[follows_user_id], lazy="joined")
+    user = relationship("User", back_populates="follows", foreign_keys=[user_id])
+    follows_user = relationship("User", back_populates="followers", foreign_keys=[follows_user_id])
 
 
 class Tweet(Base):
@@ -43,9 +43,9 @@ class Tweet(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     user_id = Column(Integer, ForeignKey("users.id"))
 
-    user = relationship("User", back_populates="tweets", foreign_keys=[user_id], lazy="joined")
-    comments = relationship("Comments", back_populates="tweet", lazy="joined")
-    likes = relationship("TweetLikes", back_populates="tweet", lazy="joined")
+    user = relationship("User", back_populates="tweets", foreign_keys=[user_id])
+    comments = relationship("Comments", back_populates="tweet")
+    likes = relationship("TweetLikes", back_populates="tweet")
 
 
 class TweetLikes(Base):
@@ -55,8 +55,8 @@ class TweetLikes(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     tweet_id = Column(Integer, ForeignKey("tweets.id"))
     
-    user = relationship("User", back_populates="tweet_likes", foreign_keys=[user_id], lazy="joined")
-    tweet = relationship("Tweet", back_populates="likes", foreign_keys=[tweet_id], lazy="joined")
+    user = relationship("User", back_populates="tweet_likes", foreign_keys=[user_id])
+    tweet = relationship("Tweet", back_populates="likes", foreign_keys=[tweet_id])
 
 
 
@@ -69,9 +69,9 @@ class Comments(Base):
     content = Column(String, index=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
-    user = relationship("User", back_populates="comments", foreign_keys=[user_id], lazy="joined")
-    tweet = relationship("Tweet", back_populates="comments", foreign_keys=[tweet_id], lazy="joined")
-    likes = relationship("CommentLikes", back_populates="comment", foreign_keys="CommentLikes.comment_id", lazy="joined")
+    user = relationship("User", back_populates="comments", foreign_keys=[user_id])
+    tweet = relationship("Tweet", back_populates="comments", foreign_keys=[tweet_id])
+    likes = relationship("CommentLikes", back_populates="comment", foreign_keys="CommentLikes.comment_id")
 
 
 class CommentLikes(Base):
@@ -81,6 +81,6 @@ class CommentLikes(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     comment_id = Column(Integer, ForeignKey("comments.id"))
     
-    user = relationship("User", back_populates="comment_likes", foreign_keys=[user_id], lazy="joined")
-    comment = relationship("Comments", back_populates="likes", foreign_keys=[comment_id], lazy="joined")
+    user = relationship("User", back_populates="comment_likes", foreign_keys=[user_id])
+    comment = relationship("Comments", back_populates="likes", foreign_keys=[comment_id])
 
