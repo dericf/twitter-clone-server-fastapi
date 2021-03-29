0d7190e226fcb8d06beb3df6d35c85d928bb0eed
Deric F <dericgf@gmail.com>
2021-03-28 23:47:35 -0600

    Modify OAuth Scheme to allow Header and Cookie Auth

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,6 +1,11 @@
 # FastAPI
 from fastapi import Depends, status, HTTPException
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+
+from starlette.responses import RedirectResponse, Response, JSONResponse
+from starlette.requests import Request
 
 # SQLAlchemy
 from sqlalchemy.orm.session import Session
@@ -8,13 +13,63 @@ from sqlalchemy.orm.session import Session
 # JWT
 from jose import JWTError, jwt
 
+from typing import Optional
+
 # Custom Modules
 from . import crud, schemas
 from .database import SessionLocal
 from .core import security
 from .core.config import settings
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+class OAuth2PasswordBearerCookie(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: str = None,
+        scopes: dict = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": {}})
+        super().__init__(flows=flows, scheme_name=scheme_name, auto_error=auto_error)
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        header_authorization: str = request.headers.get("Authorization")
+        cookie_authorization: str = request.cookies.get("Authorization")
+
+        header_scheme, header_param = get_authorization_scheme_param(
+            header_authorization
+        )
+        cookie_scheme, cookie_param = get_authorization_scheme_param(
+            cookie_authorization
+        )
+
+        if header_scheme.lower() == "bearer":
+            authorization = True
+            scheme = header_scheme
+            param = header_param
+
+        elif cookie_scheme.lower() == "bearer":
+            authorization = True
+            scheme = cookie_scheme
+            param = cookie_param
+
+        else:
+            authorization = False
+
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated"
+                )
+            else:
+                return None
+        return param
+
+# oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearerCookie(tokenUrl="/token")
 
 
 def get_db():
