8f5e202cf7fd632a19015984a67884b2ccffe4fa
Deric F <dericgf@gmail.com>
2021-04-04 22:52:05 -0600

    Implement proper user update endpoint + rename and comment /users/ endpoint for more clairity

--- api/routers/users.py
+++ api/routers/users.py
@@ -16,25 +16,26 @@ from .. import background_functions
 
 router = APIRouter(prefix="/users", tags=['users'])
 
+
 @router.get("/", response_model=List[schemas.UserResponse])
-def get_all_users(userId: Optional[int] = None, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    """Return a list of all users in the database
+def get_one_or_all_users(userId: Optional[int] = None, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """Return either all users, or a single user with id == userId. Always returns a list.
     """
     if userId:
         users = [crud.get_user_by_id(db, userId)]
     else:
         users = crud.get_users(db, skip=skip, limit=limit)
-    
+
     # TODO perhaps there is a better way of returning this model.
     # It seems like its trying to immidate graphql
     return [
-        schemas.UserResponse(  
-        id=user.id,
-        email=user.email,
-        username=user.username,
-        bio=user.bio,
-        birthdate=user.birthdate
-    ) for user in users]
+        schemas.UserResponse(
+            id=user.id,
+            email=user.email,
+            username=user.username,
+            bio=user.bio,
+            birthdate=user.birthdate
+        ) for user in users]
 
 
 @router.get("/me", response_model=schemas.User)
@@ -52,30 +53,43 @@ async def create_user(user: schemas.UserCreate, bg_tasks: BackgroundTasks, db: S
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
     print("Sending registration email")
-    bg_tasks.add_task(background_functions.send_registration_confirmation_email, email=user.email)
+    bg_tasks.add_task(
+        background_functions.send_registration_confirmation_email, email=user.email)
     return crud.create_user(db=db, user=user)
 
 
 @router.put('/', response_model=schemas.UserUpdateResponseBody)
-def update_user(request_body:schemas.UserUpdateRequestBody,
-                 db: Session = Depends(get_db),
-                 current_user: schemas.UserWithPassword = Depends(get_current_user)):
-    
+def update_user(request_body: schemas.UserUpdateRequestBody,
+                db: Session = Depends(get_db),
+                current_user: schemas.UserWithPassword = Depends(get_current_user)):
+    """Update an authenticated user's username and/or bio.
+    """
     # Check that password is correct
     if not security.verify_password(request_body.password, current_user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not authorized to update that user")
-    
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
+
+    # Check if they are trying to update the username
+    if request_body.newUsername is not None:
+        # see if that username is available
+        db_user_with_username = crud.get_user_by_username(
+            db, request_body.newUsername)
+        if db_user_with_username is not None:
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Username already exists")
+
     # Update user attributes
     user = crud.update_user(db, current_user.id, request_body)
     return user
 
 
 @router.delete('/', response_model=schemas.EmptyResponse)
-def delete_user(request_body:schemas.UserDeleteRequestBody,
-                 db: Session = Depends(get_db),
-                 current_user: schemas.UserWithPassword = Depends(get_current_user)):
+def delete_user(request_body: schemas.UserDeleteRequestBody,
+                db: Session = Depends(get_db),
+                current_user: schemas.UserWithPassword = Depends(get_current_user)):
     if not security.verify_password(request_body.password, current_user.hashed_password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not authorized to delete that user")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
     delete_successful = crud.delete_user(db, current_user.id)
-    
+
     return schemas.EmptyResponse()
